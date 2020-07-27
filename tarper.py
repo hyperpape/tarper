cedbb733e77f234ef727cf62d15d3c402be39426
Justin Blank <justin.blank@gmail.com>
2020-07-26 23:28:23 -0400

    Try similar tokens

--- tarper.py
+++ tarper.py
@@ -1,3 +1,4 @@
+import collections
 import itertools
 import os
 import random
@@ -31,8 +32,23 @@ def permute_within_directory(path):
         for elem in elems:
             yield elem
 
+def determine_similar_files(path):
+    files = candidate_files(path)
+    return sorted(list(files), key=lambda f: most_common(os.path.join(f[0], f[1])))
+
+def most_common(file):
+    return collections.Counter(words(file)).most_common()
+
+def words(file):
+    with open(file, 'r') as f:
+        for line in f.readlines():
+            for word in line.split(' .;\(\){}[]'):
+                yield word
+
 if __name__ == '__main__':
     target_archive = sys.argv[1]
     src = "/home/justin/Downloads/StringMatching"
     make_archive(target_archive + '.tar', random_order(src))
     make_archive(target_archive + 'subdir.tar', permute_within_directory(src))
+    make_archive(target_archive + 'similar.tar', determine_similar_files(src))
+
