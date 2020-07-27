df37120baa8603e7625af7e5bd1daf78c9a3639b
Justin Blank <justin.blank@gmail.com>
2020-07-26 23:03:58 -0400

    Try shuffling only within directories

--- tarper.py
+++ tarper.py
@@ -1,3 +1,4 @@
+import itertools
 import os
 import random
 import subprocess
@@ -9,21 +10,29 @@ def candidate_files(path):
             yield parent, file
         
 def make_archive(name, files):
-    first = True
-    for file in files:
-        if first:
-            first = False
-            subprocess.call(['tar', '-cf', name, file])
-        else:
-            subprocess.call(['tar', '-rf', name, file])
+    files = [os.path.join(file_pair[0], file_pair[1]) for file_pair in files]
+    first, *rest = files
+    subprocess.call(['tar', '-cf', name, first])
+    for file in rest:
+        subprocess.call(['tar', '-rf', name, file])
     subprocess.call(['gzip', name])
             
 def random_order(path):
     files = list(candidate_files(path))
     random.shuffle(files)
-    return [os.path.join(file_pair[0], file_pair[1]) for file_pair in files]
+    return files
+
+def permute_within_directory(path):
+    files = candidate_files(path)
+    sublists = itertools.groupby(files, lambda x: x[0])
+    for grouper in sublists:
+        elems = list(grouper[1])
+        random.shuffle(elems)
+        for elem in elems:
+            yield elem
 
 if __name__ == '__main__':
     target_archive = sys.argv[1]
-    make_archive(target_archive, random_order("/home/justin/Downloads/StringMatching"))
-    
+    src = "/home/justin/Downloads/StringMatching"
+    make_archive(target_archive + '.tar', random_order(src))
+    make_archive(target_archive + 'subdir.tar', permute_within_directory(src))
