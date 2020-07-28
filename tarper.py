aa7fee3b78c5ad8d3310c551de30e734293f7d26
Justin Blank <justin.blank@gmail.com>
2020-07-27 20:44:15 -0400

    Further swapping impls

--- tarper.py
+++ tarper.py
@@ -47,26 +47,38 @@ def words(file):
 
 def sort_by_size(path):
     files = candidate_files(path)
-    return sorted(list(files), key=lambda f: os.path.getsize(os.path.join(f[0], f[1])))
+    return sorted(list(files),
+                  key=lambda f: os.path.getsize(os.path.join(f[0], f[1])))
 
 def by_swapping(path):
-    best_choice = list(candidate_files(path))
+    return by_swapping_files(list(candidate_files(path)))
+
+def by_swapping_with_permutation(path):
+    return by_swapping_files(list(permute_within_directory(path)))
+
+def by_swapping_files(best_choice):
     make_archive('tmpfile.tar', best_choice)
     best_size = os.path.getsize('tmpfile.tar.gz')
     os.remove('tmpfile.tar.gz')
-    for i in range(len(best_choice)):
+    i = 0
+    iters = 0;
+    # making sure it's at least a gap of two, because it seems as if the files 
+    # generated here sometimes differ by one byte--no idea why
+    while i + 1 < len(best_choice):
         next_choice = list(best_choice)
-        if i + 1 < len(next_choice):
-            left = next_choice[i]
-            right = next_choice[i + 1]
-            next_choice[i] = right
-            next_choice[i + 1] = left
-            make_archive('tmpfile.tar', next_choice)
-            size = os.path.getsize('tmpfile.tar.gz')
-            os.remove('tmpfile.tar.gz')
-            if size < best_size:
-                best_choice = next_choice
-                best_size = size
+        left = next_choice[i]
+        right = next_choice[i + 1]
+        next_choice[i] = right
+        next_choice[i + 1] = left
+        make_archive('tmpfile.tar', next_choice)
+        size = os.path.getsize('tmpfile.tar.gz')
+        os.remove('tmpfile.tar.gz')
+        if size + 1 < best_size:
+            best_choice = next_choice
+            best_size = size
+            i = max(0, i - 3) # yolo, think harder about bounds 
+            iters += 1
+        i += 1
     return best_choice
 
 if __name__ == '__main__':
@@ -74,9 +86,11 @@ if __name__ == '__main__':
     src = "/home/justin/Downloads/StringMatching"
     make_archive(target_archive + '.tar', random_order(src))
     make_archive(target_archive + 'subdir.tar', permute_within_directory(src))
+    make_archive(target_archive + 'byswappingperm.tar', by_swapping_with_permutation(src))
     if target_archive == '1':
         # deterministic, don't redo
         make_archive(target_archive + 'default.tar', candidate_files(src))
-        make_archive(target_archive + 'similar.tar', determine_similar_files(src))
+        make_archive(target_archive + 'similar.tar',
+                     determine_similar_files(src))
         make_archive(target_archive + 'bysize.tar', sort_by_size(src))
         make_archive(target_archive + 'byswapping.tar', by_swapping(src))
